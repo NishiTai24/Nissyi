10eaab20c710ddad2d0eaeb7cc7ed1adff076c0a
NishiTai24 <nissy24244@gmail.com>
2024-11-09 20:48:19 +0900

    画像モノクロ化

---
+++ gazoumonokuro.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+import tkinter.filedialog as fd
+from PIL import Image, ImageTk
+
+def dispPhoto(path):
+    newImage = Image.open(path).convert("L").resize((300, 300))  
+    imageData = ImageTk.PhotoImage(newImage)        
+    imageLabel.configure(image=imageData)           
+    imageLabel.image = imageData                    
+
+def openFile():
+    fpath = fd.askopenfilename()                     
+    if fpath:                                        
+        dispPhoto(fpath)                             
+
+root = tk.Tk()
+root.geometry("400x350")
+
+btn = tk.Button(root, text="ファイルを開く", command=openFile)  
+imageLabel = tk.Label(root)                                 
+
+btn.pack()
+imageLabel.pack()
+
+root.mainloop()
--- scikit-learn.py
+++ scikit-learn.py
@@ -2,3 +2,12 @@ pythob3 -m pip  install numpy
 pythob3 -m pip  install scipy
 pythob3 -m pip  install scikit-learn
 pythob3 -m pip  install matplotlib
+
+
+import sklean.datasets
+
+digits = sklean.datasets.load_digits()
+
+print("データの個数",len(digits.images))
+print("画像データ",len(digits.images))
+print("データの個数",len(digits.images))
