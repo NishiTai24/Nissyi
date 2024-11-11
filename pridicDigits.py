9892cbd9fcf342349538a01516bc067ded6b4750
NishiTai24 <nissy24244@gmail.com>
2024-11-11 21:18:22 +0900

    画像ファイルから数字よ予測するプログラム

---
+++ pridicDigits.py
@@ -0,0 +1,37 @@
+import sklearn.datasets
+import sklearn.svm
+import PIL.Image
+import numpy
+
+# 画像ファイルを数値リストに変換する
+def imageToData(filename):
+    # 画像を8x8のグレースケールに変換
+    grayImage = PIL.Image.open(filename).convert("L")
+    grayImage = grayImage.resize((8, 8), PIL.Image.Resampling.LANCZOS)
+    # 数値リストに変換
+    numImage = numpy.asarray(grayImage, dtype=float)
+    numImage = 16 - numpy.floor(17 * numImage / 256)
+    numImage = numImage.flatten()
+
+    return numImage
+
+# 数字を予測する
+def predictDigits(data):
+    # 学習用データを読み込む
+    digits = sklearn.datasets.load_digits()
+    # 機械学習
+    clf = sklearn.svm.SVC(gamma=0.001)
+    clf.fit(digits.data, digits.target)
+    # 予測する結果を表示する
+    n = clf.predict([data])
+    print("予測:", n)
+
+# 画像ファイルを数値リストに変換する
+data = imageToData("2.png")
+# 数字を予測する
+predictDigits(data)
+
+
+from PIL import Image
+image = Image.new("L", (8, 8), color=255)  # 8x8の白い画像
+image.save("/Users/nissy/Documents/2.png")
